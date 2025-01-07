5a9de667d67d6c7f99466a2bc6c0003d507f908c
Tony Theodoropoulos <theodot@outlook.com>
2025-01-07 09:47:31 -0500

    updated comments

---
+++ Catch/Form1-LAPTOP-6GLJHUNS.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Catch
+{
+    public partial class Form1 : Form
+    {
+        //Hero variables
+        Rectangle hero = new Rectangle(280, 540, 40, 10);
+        int heroSpeed = 10;
+
+        //Ball variables
+        int ballSize = 10;
+        int ballSpeed = 8;
+
+        //List of balls
+
+
+        int score = 0;
+        int time = 500;
+
+        bool leftPressed = false;
+        bool rightPressed = false;
+
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        Random randGen = new Random();
+        int randValue = 0;
+
+        int groundHeight = 50;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftPressed = true;
+                    break;
+                case Keys.Right:
+                    rightPressed = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftPressed = false;
+                    break;
+                case Keys.Right:
+                    rightPressed = false;
+                    break;
+            }
+        }
+
+
+        private void gameTime_Tick(object sender, EventArgs e)
+        {
+
+
+            //redraw the screen
+            Refresh();
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //update labels
+            timeLabel.Text = $"Time Left: {time}";
+            scoreLabel.Text = $"Score: {score}";
+
+            //draw ground
+            e.Graphics.FillRectangle(greenBrush, 0, this.Height - groundHeight, this.Width, groundHeight);
+
+            //draw hero
+            e.Graphics.FillRectangle(whiteBrush, hero);
+
+            //draw balls
+        }
+    }
+}
